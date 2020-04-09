543d6d0d19ea2e3d5b54cb43efc76a9434223f87
Alexander Savchenko <sasha.savchenko@gmail.com>
2020-04-10 01:43:35 +0300

    pm2

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-. /home/ubuntu/.nvm/nvm.sh
-/home/ubuntu/.nvm/versions/node/v12.16.1/bin/pm2 delete server
+/home/ubuntu/.nvm/versions/node/v12.16.1/bin/node /home/ubuntu/.nvm/versions/node/v12.16.1/bin/pm2 delete server
