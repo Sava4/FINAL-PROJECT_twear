3dc6da575590e33672905b7885bd573cd7bd1e5b
Alexander Savchenko <sasha.savchenko@gmail.com>
2020-04-10 00:42:13 +0300

    nvm

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-export NVM_DIR="/home/ubuntu/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+set -e
+. /home/ubuntu/.nvm/nvm.sh
+command -v nvm
 pm2 delete server
