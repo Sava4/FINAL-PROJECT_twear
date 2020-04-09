290874aca0fb62595e8694f9ceb4088cc1c8df67
Alexander Savchenko <sasha.savchenko@gmail.com>
2020-04-09 23:13:11 +0300

    add bashrc

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+source /home/ubuntu/.bashrc
 pm2 delete server
