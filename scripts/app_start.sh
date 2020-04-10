703341e4d98fa6e6e182cee4d6665f2dfe261d36
Alexander Savchenko <sasha.savchenko@gmail.com>
2020-04-10 17:49:45 +0300

    server js update

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /var/www/FEM4Final
-runuser -l ubuntu -c 'pm2 start server.js -i 2 --exp-backoff-restart-delay=500'
+runuser -l ubuntu -c 'pm2 start /var/www/FEM4Final/server.js -i 2 --exp-backoff-restart-delay=500'
